6b64224294aee20ca5ab3d2e3e6d743224ec2bbb
AZagatti <andre.zagatti1@gmail.com>
2019-07-30 14:48:49 -0300

    Adicionado o projeto

---
+++ app_send_mail_public/processa_envio.php
@@ -0,0 +1,11 @@
+<?php 
+
+	require "../app_send_mail_private/bibliotecas/PHPMailer/Exception.php";
+	require "../app_send_mail_private/bibliotecas/PHPMailer/OAuth.php";
+	require "../app_send_mail_private/bibliotecas/PHPMailer/PHPMailer.php";
+	require "../app_send_mail_private/bibliotecas/PHPMailer/POP3.php";
+  require "../app_send_mail_private/bibliotecas/PHPMailer/SMTP.php";
+
+	require "../app_send_mail_private/processa_envio.php";
+
+?>
